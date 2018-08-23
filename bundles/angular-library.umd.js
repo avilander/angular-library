60a61eb1b9be77a49827723be378c98cda1bcda7
PXTUDI\avilandersc <avilandersc@peixoto.com.br>
2018-08-23 11:23:21 -0300

    Correlçao

--- bundles/angular-library.umd.js
+++ bundles/angular-library.umd.js
@@ -1,100 +1,100 @@
 (function (global, factory) {
-    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@angular/core'), require('@angular/platform-browser'), require('@angular/platform-browser/animations'), require('@angular/material'), require('@angular/cdk/layout'), require('@angular/material/toolbar'), require('@angular/common')) :
-    typeof define === 'function' && define.amd ? define(['exports', '@angular/core', '@angular/platform-browser', '@angular/platform-browser/animations', '@angular/material', '@angular/cdk/layout', '@angular/material/toolbar', '@angular/common'], factory) :
-    (factory((global['angular-library'] = {}),global.ng.core,null,null,null,null,null,null));
-}(this, (function (exports,core,platformBrowser,animations,material,layout,toolbar,common) { 'use strict';
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@angular/platform-browser'), require('@angular/core'), require('@angular/platform-browser/animations'), require('@angular/material'), require('@angular/cdk/layout'), require('@angular/material/toolbar'), require('@angular/common')) :
+    typeof define === 'function' && define.amd ? define(['exports', '@angular/platform-browser', '@angular/core', '@angular/platform-browser/animations', '@angular/material', '@angular/cdk/layout', '@angular/material/toolbar', '@angular/common'], factory) :
+    (factory((global['angular-library'] = global['angular-library'] || {}),global._angular_platformBrowser,global.ng.core,global._angular_platformBrowser_animations,global._angular_material,global._angular_cdk_layout,global._angular_material_toolbar,global._angular_common));
+}(this, (function (exports,_angular_platformBrowser,_angular_core,_angular_platformBrowser_animations,_angular_material,_angular_cdk_layout,_angular_material_toolbar,_angular_common) { 'use strict';
 
-    var AppComponent = /** @class */ (function () {
-        function AppComponent() {
-            this.title = 'al';
-        }
-        AppComponent.decorators = [
-            { type: core.Component, args: [{
-                        selector: 'al-root',
-                        templateUrl: './app.component.html',
-                        styleUrls: ['./app.component.css']
-                    },] },
-        ];
-        return AppComponent;
-    }());
+var AppComponent = /** @class */ (function () {
+    function AppComponent() {
+        this.title = 'al';
+    }
+    AppComponent.decorators = [
+        { type: _angular_core.Component, args: [{
+                    selector: 'al-root',
+                    templateUrl: './app.component.html',
+                    styleUrls: ['./app.component.css']
+                },] },
+    ];
+    return AppComponent;
+}());
 
-    var AppModule = /** @class */ (function () {
-        function AppModule() {
-        }
-        AppModule.decorators = [
-            { type: core.NgModule, args: [{
-                        declarations: [
-                            AppComponent
-                        ],
-                        imports: [
-                            platformBrowser.BrowserModule,
-                            animations.BrowserAnimationsModule,
-                            layout.LayoutModule,
-                            material.MatToolbarModule,
-                            material.MatButtonModule,
-                            material.MatSidenavModule,
-                            material.MatIconModule,
-                            material.MatListModule,
-                            material.MatCardModule
-                        ],
-                        providers: [],
-                        bootstrap: [AppComponent]
-                    },] },
-        ];
-        return AppModule;
-    }());
+var AppModule = /** @class */ (function () {
+    function AppModule() {
+    }
+    AppModule.decorators = [
+        { type: _angular_core.NgModule, args: [{
+                    declarations: [
+                        AppComponent
+                    ],
+                    imports: [
+                        _angular_platformBrowser.BrowserModule,
+                        _angular_platformBrowser_animations.BrowserAnimationsModule,
+                        _angular_cdk_layout.LayoutModule,
+                        _angular_material.MatToolbarModule,
+                        _angular_material.MatButtonModule,
+                        _angular_material.MatSidenavModule,
+                        _angular_material.MatIconModule,
+                        _angular_material.MatListModule,
+                        _angular_material.MatCardModule
+                    ],
+                    providers: [],
+                    bootstrap: [AppComponent]
+                },] },
+    ];
+    return AppModule;
+}());
 
-    var PxtAppHeaderComponent = /** @class */ (function () {
-        function PxtAppHeaderComponent() {
-        }
-        PxtAppHeaderComponent.decorators = [
-            { type: core.Component, args: [{
-                        selector: 'pxt-header',
-                        styles: [''],
-                        template: "<mat-toolbar color=\"primary\">\n        <span>Peixoto NavBar.</span>\n        <!-- This fills the remaining space of the current row -->\n        <span class=\"example-fill-remaining-space\"></span>\n        <span></span>\n    </mat-toolbar>"
-                    },] },
-        ];
-        /** @nocollapse */
-        PxtAppHeaderComponent.ctorParameters = function () { return []; };
-        return PxtAppHeaderComponent;
-    }());
+var PxtAppHeaderComponent = /** @class */ (function () {
+    function PxtAppHeaderComponent() {
+    }
+    PxtAppHeaderComponent.decorators = [
+        { type: _angular_core.Component, args: [{
+                    selector: 'pxt-header',
+                    styles: [''],
+                    template: "<mat-toolbar color=\"primary\">\n        <span>Peixoto NavBar.</span>\n        <!-- This fills the remaining space of the current row -->\n        <span class=\"example-fill-remaining-space\"></span>\n        <span></span>\n    </mat-toolbar>"
+                },] },
+    ];
+    /** @nocollapse */
+    PxtAppHeaderComponent.ctorParameters = function () { return []; };
+    return PxtAppHeaderComponent;
+}());
 
-    var PxtAppHeaderModule = /** @class */ (function () {
-        function PxtAppHeaderModule() {
-        }
-        PxtAppHeaderModule.decorators = [
-            { type: core.NgModule, args: [{
-                        imports: [common.CommonModule,
-                            platformBrowser.BrowserModule,
-                            animations.BrowserAnimationsModule,
-                            layout.LayoutModule,
-                            toolbar.MatToolbarModule,
-                            material.MatButtonModule,
-                            material.MatSidenavModule,
-                            material.MatIconModule,
-                            material.MatListModule,
-                            material.MatCardModule],
-                        declarations: [PxtAppHeaderComponent],
-                        exports: [PxtAppHeaderComponent],
-                        schemas: [core.CUSTOM_ELEMENTS_SCHEMA, core.NO_ERRORS_SCHEMA],
-                        providers: [],
-                        bootstrap: [PxtAppHeaderComponent]
-                    },] },
-        ];
-        return PxtAppHeaderModule;
-    }());
+var PxtAppHeaderModule = /** @class */ (function () {
+    function PxtAppHeaderModule() {
+    }
+    PxtAppHeaderModule.decorators = [
+        { type: _angular_core.NgModule, args: [{
+                    imports: [_angular_common.CommonModule,
+                        _angular_platformBrowser.BrowserModule,
+                        _angular_platformBrowser_animations.BrowserAnimationsModule,
+                        _angular_cdk_layout.LayoutModule,
+                        _angular_material_toolbar.MatToolbarModule,
+                        _angular_material.MatButtonModule,
+                        _angular_material.MatSidenavModule,
+                        _angular_material.MatIconModule,
+                        _angular_material.MatListModule,
+                        _angular_material.MatCardModule],
+                    declarations: [PxtAppHeaderComponent],
+                    exports: [PxtAppHeaderComponent],
+                    schemas: [_angular_core.CUSTOM_ELEMENTS_SCHEMA, _angular_core.NO_ERRORS_SCHEMA],
+                    providers: [],
+                    bootstrap: [PxtAppHeaderComponent]
+                },] },
+    ];
+    return PxtAppHeaderModule;
+}());
 
-    /**
-     * @module
-     * @description
-     * Entry point for all public APIs of the async local storage package.
-     */
+/**
+ * @module
+ * @description
+ * Entry point for all public APIs of the async local storage package.
+ */
 
-    exports.AppModule = AppModule;
-    exports.AppComponent = AppComponent;
-    exports.PxtAppHeaderModule = PxtAppHeaderModule;
-    exports.PxtAppHeaderComponent = PxtAppHeaderComponent;
+exports.AppModule = AppModule;
+exports.AppComponent = AppComponent;
+exports.PxtAppHeaderModule = PxtAppHeaderModule;
+exports.PxtAppHeaderComponent = PxtAppHeaderComponent;
 
-    Object.defineProperty(exports, '__esModule', { value: true });
+Object.defineProperty(exports, '__esModule', { value: true });
 
 })));
